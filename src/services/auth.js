d65200348572d78d807bcab4253188676a892892
Akalanka <akalankaperera128@gmail.com>
2022-08-14 22:26:30 +0530

    Fixed api request code

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,10 +1,5 @@
-import axios from 'axios'
+import { axiosInstance, apiRequest } from './core/axios'
 
 export const login = async (data) => {
-  return await axios
-    .post(`${import.meta.env.VITE_BASHAWAY_BE_URL}/api/auth/login`, data)
-    .then((res) => res.data)
-    .catch((error) => {
-      alert(error.response.data.message)
-    })
+  return await apiRequest(() => axiosInstance.post(`/api/auth/login`, data))
 }
