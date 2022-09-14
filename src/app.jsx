8773d6a8bddc7fa1d7ec5120a956173846e57101
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2022-09-14 16:08:48 +0530

    Route transitions added

--- src/app.jsx
+++ src/app.jsx
@@ -1,32 +1,14 @@
-import Login from './pages/login'
-import Register from './pages/register'
-import Home from './pages/home'
-import QuestionView from './pages/questionView'
-import ForgotPassword from './pages/forgotPassword'
-import ResetPassword from './pages/resetPassword'
-import ChangePassword from './pages/changePassword'
-import Profile from './pages/profile'
-import NotFound from './pages/404'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
+import AnimatedRoutes from './components/AnimatedRoutes'
 
 const App = () => {
   return (
     <Provider store={store}>
       <div className="App">
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/question" element={<QuestionView />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/reset-password/:code" element={<ResetPassword />} />
-            <Route path="/change-password" element={<ChangePassword />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <AnimatedRoutes />
         </BrowserRouter>
       </div>
     </Provider>
