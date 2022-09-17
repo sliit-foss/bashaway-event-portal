312230c805c1a0c6f49cf0f3be0359a6813fd575
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-09-17 15:50:15 +0530

    Add logos to the footer

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -64,11 +64,26 @@ const Footer = () => {
   return (
     <>
       <Divider />
-      <div className="flex flex-col justify-between p-10 bg-transparent md:flex-row md:p-12 relative z-[2]" id="footer">
-        <div className="flex justify-center w-full pl-0 md:pl-24 md:justify-start">
-          <a href="https://sliitfoss.org" target="_blank" rel="noreferrer">
-            <img src="/assets/images/foss_light.svg" className="w-28 h-28" />
-          </a>
+      <div className="flex flex-col justify-between p-10 bg-transparent md:flex-row md:p-12 relative z-[2] grayscale " id="footer">
+        <div className="w-full">
+          <div className="flex flex-col justify-center items-center w-full xl:w-10/12 md:pl-24 md:items-center md:justify-start scale-75 relative md:bottom-8">
+            <div className="flex justify-start items-center">
+              <a href="https://sliitfoss.org" target="_blank" rel="noreferrer">
+                <img src="/assets/images/foss-logo.svg" className="w-[6.6rem] h-[6.6rem] mx-6" />
+              </a>
+              <a href="https://www.facebook.com/sliit.fcsc/" target="_blank" rel="noreferrer">
+                <img src="/assets/images/fcsc-logo.png" className="w-32 h-32 mx-6 filter brightness-125" />
+              </a>
+            </div>
+            <div className="flex justify-start items-center">
+              <a href="https://community.mozilla.org/en/groups/mozilla-campus-club-of-sliit/" target="_blank" rel="noreferrer">
+                <img src="/assets/images/mozilla-logo.png" className="w-[6.5rem] h-[6.5rem] mx-6 mr-8 filter brightness-115" />
+              </a>
+              <a href="https://wif-web.web.app" target="_blank" rel="noreferrer">
+                <img src="/assets/images/wif-logo.png" className="w-28 h-28 ml-4 mx-6" />
+              </a>
+            </div>
+          </div>
         </div>
         <div className="flex flex-col items-center justify-center w-full mt-6 md:justify-start md:items-center">
           <h1 className="mb-8 text-2xl font-semibold text-white">Contact</h1>
