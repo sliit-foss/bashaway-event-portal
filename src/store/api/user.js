8e7755d7f80d65f7c628271c8fcc7b38b6b70cde
Akalanka <akalankaperera128@gmail.com>
2023-08-02 15:54:22 +0530

    Fix: service http methods

--- src/store/api/user.js
+++ src/store/api/user.js
@@ -1,17 +1,17 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 import baseQuery, { mutationHelper } from "./base";
 
-const { put } = mutationHelper;
+const { patch } = mutationHelper;
 
 export const userApi = createApi({
   reducerPath: "userApi",
   baseQuery,
   endpoints: (builder) => ({
     updateProfile: builder.mutation({
-      query: ({ id, data }) => put(`/api/users/${id}`, data)
+      query: ({ id, data }) => patch(`/api/users/${id}`, data)
     }),
     changePassword: builder.mutation({
-      query: (data) => put(`/api/users/change_password`, data)
+      query: (data) => patch(`/api/users/change_password`, data)
     })
   })
 });
