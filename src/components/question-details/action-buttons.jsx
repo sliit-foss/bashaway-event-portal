a5d7f12efd12b7962b917f7dc838912c0ef2d132
Akalanka <akalankaperera128@gmail.com>
2023-08-22 01:58:27 +0530

    Feat: blocked submission button as per deadline

--- src/components/question-details/action-buttons.jsx
+++ src/components/question-details/action-buttons.jsx
@@ -2,18 +2,19 @@ import { Plus } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { twMerge } from "tailwind-merge";
 import { Button } from "@/components/common";
+import { useGetSettingsQuery } from "@/store/api";
 import { downloadFile } from "@/utils";
 
 const ActionButtons = ({ question, className, buttonClassName }) => {
+  const { data: { data: settings } = {}, isLoading } = useGetSettingsQuery();
+
   const navigate = useNavigate();
   return (
     <div className={twMerge("flex flex-col md:flex-row gap-3 mt-1", className)}>
       <Button
         className={twMerge("py-2 md:py-1.5", buttonClassName)}
-        disabled={false}
-        onClick={() => {
-          document.getElementById("file-upload").click();
-        }}
+        disabled={isLoading || new Date(settings?.submission_deadline) < new Date()}
+        onClick={() => document.getElementById("file-upload").click()}
       >
         <Plus strokeWidth="2.5" />
         Add Submission
