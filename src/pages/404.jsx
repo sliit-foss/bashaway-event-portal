918015b9381ebfa0bf11eb7ef6fa538fdcc9b423
Akalanka <akalankaperera128@gmail.com>
2023-08-22 02:15:04 +0530

    Fix: 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,18 +4,18 @@ import { useTitle } from "@/hooks";
 const NotFound = () => {
   useTitle("404 | Bashaway");
   return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center relative z-50">
+    <div className="h-[80vh] w-screen flex flex-col justify-center items-center relative z-50">
       <Watch
         height="110"
         width="110"
         radius="48"
-        color="#0070F3"
+        color="#000000"
         ariaLabel="watch-loading"
         wrapperStyle={{}}
         wrapperClassName=""
         visible={true}
       />
-      <h1 className="text-white text-6xl font-bold mt-10">404</h1>
+      <h1 className="text-black text-6xl font-bold mt-10">404</h1>
     </div>
   );
 };
