06a01d5f8c5eaae5760407fc4d9f572e52b896d2
Akalanka <akalankaperera128@gmail.com>
2023-08-25 00:40:51 +0530

    Fix: ignored sentry initialization on local environment

--- src/services/sentry.js
+++ src/services/sentry.js
@@ -1,13 +1,15 @@
 import * as Sentry from "@sentry/react";
 
 export const initializeTracing = () => {
-  Sentry.init({
-    dsn: import.meta.env.VITE_SENTRY_DSN,
-    integrations: [new Sentry.Replay()],
-    environment: import.meta.env.VITE_APP_ENV,
-    replaysSessionSampleRate: import.meta.env.VITE_APP_ENV === "production" ? 0.1 : 1.0,
-    replaysOnErrorSampleRate: import.meta.env.VITE_APP_ENV === "production" ? 0.1 : 1.0
-  });
+  if (import.meta.env.VITE_APP_ENV !== "local") {
+    Sentry.init({
+      dsn: import.meta.env.VITE_SENTRY_DSN,
+      integrations: [new Sentry.Replay()],
+      environment: import.meta.env.VITE_APP_ENV,
+      replaysSessionSampleRate: import.meta.env.VITE_APP_ENV === "production" ? 0.1 : 1.0,
+      replaysOnErrorSampleRate: import.meta.env.VITE_APP_ENV === "production" ? 0.1 : 1.0
+    });
+  }
 };
 
 export default initializeTracing;
