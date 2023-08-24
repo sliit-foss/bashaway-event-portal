3f072137f24a75748912db1877da11fd9e0b9d89
Akalanka <akalankaperera128@gmail.com>
2023-08-25 01:02:25 +0530

    Chore: made leaderboard url dynamic per env

--- src/constants/hyperlinks.js
+++ src/constants/hyperlinks.js
@@ -2,3 +2,9 @@ export const githubOrgLink = "https://github.com/sliit-foss";
 export const repositoryLink = "https://github.com/sliit-foss/bashaway-event-portal";
 export const scorekeeperRepositoryLink = "https://github.com/sliit-foss/scorekeeper";
 export const sliitFossMainWebsite = "https://sliitfoss.org";
+
+export let leaderboardURL = "https://leaderboard.bashaway.sliitfoss.org";
+
+if (import.meta.env.VITE_APP_ENV !== "production") {
+  leaderboardURL = leaderboardURL.replace("leaderboard.", `leaderboard.${import.meta.env.VITE_APP_ENV}.`);
+}
