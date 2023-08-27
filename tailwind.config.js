82a918092767d564873ed2f24578ef06d09b0057
Akalanka <akalankaperera128@gmail.com>
2023-08-27 22:09:59 +0530

    Chore: cleaned tailwind config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,42 +19,39 @@ export default {
     },
     extend: {
       colors: {
-        "border": "var(--border)",
-        "input": "hsl(var(--input))",
-        "ring": "hsl(var(--ring))",
-        "background": "hsl(var(--background))",
-        "foreground": "hsl(var(--foreground))",
-        "primary": {
+        border: "var(--border)",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))"
         },
-        "secondary": {
+        secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))"
         },
-        "destructive": {
+        destructive: {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))"
         },
-        "muted": {
+        muted: {
           DEFAULT: "hsl(var(--muted))",
           foreground: "hsl(var(--muted-foreground))"
         },
-        "accent": {
+        accent: {
           DEFAULT: "hsl(var(--accent))",
           foreground: "hsl(var(--accent-foreground))"
         },
-        "popover": {
+        popover: {
           DEFAULT: "hsl(var(--popover))",
           foreground: "hsl(var(--popover-foreground))"
         },
-        "card": {
+        card: {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))"
-        },
-        "nav-links-unselected": "#888888",
-        "gray-light": "#D9D9D9",
-        "profile-card": "rgba(255, 255, 255, 0.02)"
+        }
       },
       borderRadius: {
         lg: "var(--radius)",
