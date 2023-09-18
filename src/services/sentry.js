5ade17412c1775ca04d1622fee138a5dd06d7ef0
Akalanka <akalankaperera128@gmail.com>
2023-09-18 10:47:51 +0530

    Fix: switched off sentry masking for everything except passwords

--- src/services/sentry.js
+++ src/services/sentry.js
@@ -4,7 +4,14 @@ export const initializeTracing = () => {
   if (import.meta.env.VITE_APP_ENV !== "local") {
     Sentry.init({
       dsn: import.meta.env.VITE_SENTRY_DSN,
-      integrations: [new Sentry.Replay()],
+      integrations: [
+        new Sentry.Replay({
+          maskAllText: false,
+          maskAllInputs: false,
+          networkRequestHeaders: ["x-user-email"],
+          networkResponseHeaders: ["x-correlation-id"]
+        })
+      ],
       environment: import.meta.env.VITE_APP_ENV,
       replaysSessionSampleRate: 0,
       replaysOnErrorSampleRate: 1.0
