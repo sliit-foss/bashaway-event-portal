0c8d5d238934ea9d0bd56abb7252b81ea54fe303
chamodshehanka <hcsperera@outlook.com>
2023-11-17 10:30:29 +0530

    Fix: FAQ

---
+++ src/constants/faqs.js
@@ -0,0 +1,59 @@
+export const faqs = [
+  {
+    question: "What is GDG DevFest Sri Lanka?",
+    answer:
+      "GDG DevFest Sri Lanka is a community-organized developer conference that is part of the global Google Developer Groups (GDG) DevFest series. It brings together developers, designers, and technology enthusiasts to learn, share, and network. DevFest Sri Lanka typically features a variety of technical sessions, workshops, and keynote presentations on cutting-edge technologies, platforms, and development practices."
+  },
+  {
+    question: "Where will GDG DevFest Sri Lanka 2023 be held?",
+    answer: "At Bishop’s College Auditorium, Colombo 03"
+  },
+  {
+    question: "Is there a selection process for those who paid for the ticket?",
+    answer:
+      "No, there is no selection process. If you have completed the registration, including the payment for the ticket, you are eligible to attend the event."
+  },
+  {
+    question:
+      "When will participants who have paid for Google I/O Extended Sri Lanka 2023 tickets receive their official tickets?",
+    answer:
+      "Participants who have purchased tickets for Google I/O Extended Sri Lanka 2023 will receive their official event tickets via email from GDG Sri Lanka three days prior to the event."
+  },
+  {
+    question: "What are the benefits I can get from attending this event?",
+    answer:
+      "By attending this event, you will have the opportunity to acquire extensive knowledge of Google technologies from experts, network with Googlers, GDEs, and industry experts, and most importantly, have a lot of fun!"
+  },
+  {
+    question: "Is this event beneficial for beginners in the IT industry?",
+    answer:
+      "Absolutely! This event offers valuable benefits for individuals of all levels, from beginners just starting their journey to experts with extensive experience."
+  },
+  {
+    question: "Has the registration for this event been closed?",
+    answer:
+      "Yes, registration for this event is now closed as all the available slots have been filled at a rapid pace. We sincerely appreciate your tremendous enthusiasm and interest in participating."
+  },
+  {
+    question:
+      "If I have registered and obtained a ticket for the event but am unable to attend on the event day, can I transfer my ticket to someone else?",
+    answer: "Unfortunately, you cannot transfer your ticket to someone else."
+  },
+  {
+    question:
+      "Is it still possible for someone who has registered but has not yet paid for the ticket to make the payment now?",
+    answer:
+      "Unfortunately, it is not possible to make the payment for the ticket now as all the slots have been sold out."
+  },
+  {
+    question:
+      "Can I purchase the Google I/O Extended Sri Lanka 2023 participant t-shirt without being a participant in the event?",
+    answer:
+      "Unfortunately, the participant t-shirts for the event are exclusively provided to registered participants who possess a valid ticket, and they are not available for individual sale."
+  },
+  {
+    question: "Is it possible to receive a refund for the payment I made for the event ticket?",
+    answer:
+      "Unfortunately, event tickets are non-refundable as stated in the ticket guidelines on the EventsHere platform."
+  }
+];
